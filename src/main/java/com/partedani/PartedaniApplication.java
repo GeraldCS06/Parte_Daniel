f4f56bd232039a9c375beb61fbfa75e1f94dc513
DaniEspinoza05 <danieles.espinoza25@gmail.com>
2023-11-01 22:49:57 -0600

    pARTE dANIEL

---
+++ src/main/java/com/partedani/PartedaniApplication.java
@@ -0,0 +1,13 @@
+package com.partedani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartedaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartedaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partedani/PartedaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partedani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartedaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
